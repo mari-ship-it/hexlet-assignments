f58cfd235ee5072e111a54822174d90a2970fdd6
Marina Sirenko <msirenko96@gmail.com>
2025-03-27 20:07:12 +0300

    update App.java

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,9 +1,20 @@
 package exercise;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 // BEGIN
+public class App {
 
+    public static void swapKeyValue(KeyValueStorage storage) {
+
+        var entries = storage.toMap().entrySet();
+
+        for (var entry : entries) {
+            storage.unset(entry.getKey());
+            storage.set(entry.getValue(), entry.getKey());
+        }
+    }
+}
 // END
