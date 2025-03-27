0201735bd4a58f9e353edd91b2ffb19f4b5a5560
Marina Sirenko <msirenko96@gmail.com>
2025-03-27 20:06:26 +0300

    update InMemoryKV.java

--- java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
@@ -1,8 +1,35 @@
 package exercise;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.HashMap;
 
 // BEGIN
+public class InMemoryKV implements KeyValueStorage{
 
+    private final Map<String, String> dictionary;
+
+    public InMemoryKV(Map<String, String> dictionary) {
+        this.dictionary = new LinkedHashMap<>(dictionary);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        dictionary.put(key, value);
+    }
+
+    @Override
+    public String get(String key, String defaultValue) {
+        return dictionary.getOrDefault(key, defaultValue);
+    }
+
+    @Override
+    public void unset(String key) {
+        dictionary.remove(key);
+    }
+
+    @Override
+    public LinkedHashMap<String, String> toMap() {
+        return new LinkedHashMap<>(dictionary);
+    }
+}
 // END
