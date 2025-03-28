ed7fa0e4e146782e2042794e1fdbcdeeeef70f29
Marina Sirenko <msirenko96@gmail.com>
2025-03-28 09:56:32 +0300

    update App.java

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -7,10 +7,10 @@ import java.util.Map;
 // BEGIN
 public class App {
 
+
     public static void swapKeyValue(KeyValueStorage storage) {
 
         var entries = storage.toMap().entrySet();
-
         for (var entry : entries) {
             storage.unset(entry.getKey());
             storage.set(entry.getValue(), entry.getKey());
