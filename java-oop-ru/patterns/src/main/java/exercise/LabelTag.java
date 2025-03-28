7e1ab2e148cc45800c2a36402caf4a0760b84fd3
Marina Sirenko <msirenko96@gmail.com>
2025-03-28 13:27:03 +0300

    update LabelTag.java

--- java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -1,5 +1,20 @@
 package exercise;
 
 // BEGIN
+public class LabelTag implements TagInterface {
 
+    private final String label;
+    private final TagInterface tag;
+
+    public LabelTag(String label, TagInterface tag) {
+
+        this.label = label;
+        this.tag = tag;
+    }
+
+    @Override
+    public String render() {
+        return "<label>" + label + tag.render() + "</label>";
+    }
+}
 // END
