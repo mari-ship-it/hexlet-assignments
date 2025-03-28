77fb80c6fece7989895703e035890723c0e69552
Marina Sirenko <msirenko96@gmail.com>
2025-03-28 09:57:42 +0300

    update FileKV.java

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -1,5 +1,48 @@
 package exercise;
 
-// BEGIN
+import java.util.Map;
 
+// BEGIN
+public class FileKV implements KeyValueStorage {
+
+    private final String path;
+    private Map<String, String> dictionary;
+
+    public FileKV(String path, Map<String, String> dictionary) {
+        this.path = path;
+        this.dictionary = dictionary;
+        String serialize = Utils.serialize(this.dictionary);
+        Utils.writeFile(this.path, serialize);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        String json = Utils.readFile(path);
+        Map<String, String> deserialize = Utils.deserialize(json);
+        deserialize.put(key, value);
+        this.dictionary = deserialize;
+        String serialize = Utils.serialize(this.dictionary);
+        Utils.writeFile(path, serialize);
+    }
+
+    @Override
+    public void unset(String key) {
+        String json = Utils.readFile(path);
+        Map<String, String> deserialize = Utils.deserialize(json);
+        deserialize.remove(key);
+        this.dictionary = deserialize;
+        String serialize = Utils.serialize(this.dictionary);
+        Utils.writeFile(path, serialize);
+    }
+
+    @Override
+    public String get(String key, String defaultValue) {
+        return dictionary.getOrDefault(key, defaultValue);
+    }
+
+    @Override
+    public Map<String, String> toMap() {
+        return dictionary;
+    }
+}
 // END
