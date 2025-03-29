80f6df8647f2a11adc77527ee07a9135333f6790
Marina Sirenko <msirenko96@gmail.com>
2025-03-29 15:26:21 +0300

    update SingleTag.java

--- java-oop-ru/inheritance/src/main/java/exercise/SingleTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/SingleTag.java
@@ -3,5 +3,15 @@ package exercise;
 import java.util.Map;
 
 // BEGIN
+public class SingleTag extends Tag {
 
+    SingleTag(String name, Map<String, String> map) {
+        super(name, map);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
 // END
