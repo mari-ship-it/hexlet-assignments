d71c0ceccf0676be708416c09c921be2b9115463
Marina Sirenko <msirenko96@gmail.com>
2025-03-29 13:50:35 +0300

    update Circle.java

--- java-oop-ru/errors/src/main/java/exercise/Circle.java
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -1,5 +1,23 @@
 package exercise;
 
 // BEGIN
+public class Circle {
 
+    private final int radius;
+
+    public Circle(Point point, int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public double getSquare() throws NegativeRadiusException {
+        if (getRadius() < 0) {
+            throw new NegativeRadiusException("Не удалось посчитать площадь");
+        }
+        return Math.PI * radius * radius;
+    }
+}
 // END
