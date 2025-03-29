7b9bcd51c64cd8d735f32c8a9dce9bb3cb66eaf5
Marina Sirenko <msirenko96@gmail.com>
2025-03-29 15:25:30 +0300

    update PairedTag.java

--- java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
@@ -2,8 +2,32 @@ package exercise;
 
 import java.util.Map;
 import java.util.List;
-import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
 
 // BEGIN
+public class PairedTag extends Tag {
 
+    private String bodyTag;
+    List<Tag> children;
+
+    PairedTag(String name, Map<String, String> map, String bodyTag, List<Tag> children) {
+        super(name, map);
+        this.bodyTag = bodyTag;
+        this.children = children;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(super.toString()).append(bodyTag);
+
+        for (var child : children) {
+            String string = child.toString();
+            result.append(string);
+        }
+
+        result.append("</").append(name).append(">");
+        return result.toString();
+    }
+}
 // END
