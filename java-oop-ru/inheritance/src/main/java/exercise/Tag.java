18cd13491e55060968d36f43f50cc5356ebce787
Marina Sirenko <msirenko96@gmail.com>
2025-03-29 15:24:44 +0300

    update Tag.java

--- java-oop-ru/inheritance/src/main/java/exercise/Tag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/Tag.java
@@ -1,8 +1,31 @@
 package exercise;
 
-import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
 import java.util.Map;
 
 // BEGIN
+public class Tag {
 
+    protected final String name;
+    private final Map<String, String> map;
+
+    Tag(String name, Map<String, String> map) {
+        this.name = name;
+        this.map = map;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("<").append(name).append(" ");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            result.append(key).append("=\"").append(value).append("\" ");
+        }
+
+        return result.toString().trim() + ">";
+    }
+}
 // END
